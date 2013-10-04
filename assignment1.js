bae2b8abcb284aeb4e7c092a904946a8bd276a85
curriedav <curriedav@gmail.com>
2013-10-04 13:21:37 -0700

    drafts assignment1

---
+++ assignment1.js
@@ -0,0 +1,9 @@
+console.log(var a = 5);
+console.log(var b = 3);
+console.log(var c = "David".length);
+console.log(a + b + c);
+console.log((a + c + 2) / b);
+console.log( var firstName = David);
+console.log( var lastName = Currie);
+console.log( firstName + " " + lastName);
+
