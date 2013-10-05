ff400032e3cd8ecb9e1c6fdcb6aa41a75b04f9d6
curriedav <curriedav@gmail.com>
2013-10-05 11:31:59 -0700

    removes console.log from var

--- assignment1.js
+++ assignment1.js
@@ -1,9 +1,9 @@
-console.log(var a = 5);
-console.log(var b = 3);
-console.log(var c = "David".length);
+var a = 5;
+var b = 3;
+var c = "David".length);
 console.log(a + b + c);
 console.log((a + c + 2) / b);
-console.log( var firstName = David);
-console.log( var lastName = Currie);
+var firstName = David;
+var lastName = Currie;
 console.log( firstName + " " + lastName);
 
