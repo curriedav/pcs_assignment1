257843afbe2c8ac3930493bab0057b2bed3e6391
curriedav <curriedav@gmail.com>
2013-10-07 07:14:38 -0700

    debugs improper use of strings

--- assignment1.js
+++ assignment1.js
@@ -1,15 +1,15 @@
 // declare numeric var
 var a = 5;
 var b = 3;
-var c = "David".length);
+var c = "David".length;
 // use operators on numeric variables and declare new var
 var d = a + b * c;
 console.log(a + b + c);
 console.log((a + c + 2) / b);
 // declare string var
-var firstName = David;
-var lastName = Currie;
+var firstName = "David";
+var lastName = "Currie";
 // concatenate string vars
-console.log( firstName + " " + lastName);
+console.log( firstName + " " + lastName );
 // concatenate string & numeric var
 console.log( firstName + d );
